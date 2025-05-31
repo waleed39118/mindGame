56c10720438002edeeeb3d6aae08b78d37b3ad63
waleed39118 <waleed.moltaka3@gmail.com>
2025-05-31 19:14:08 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const allCards = document.querySelectorAll(".card");
+
+let firstCard = null;
+let secondCard = null;
+let canClick = true;
+
+let matchedPairs = 0;
+const totalPairs = allCards.length / 2;
+        //the double cards
+allCards.forEach((card) => {
+  card.addEventListener("click", handleCardClicked);
+});
+//main function for i call any card
+function handleCardClicked() { 
+//! = false
+//return = dont do any action and prevent card double click
+  if(!canClick || this.classList.contains("flip")) return;
+  // إضافة الكلاس flip لقلب البطاقة
+    this.classList.add("flip");
+
+  if(!firstCard) {
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+
+const img1 = firstCard.querySelector(".front img").src;
+const img2 = secondCard.querySelector(".front img").src;
+//handel matching condiotion
+
+if (img1 === img2) {
+    matchedPairs++;
+    firstCard = null;
+    secondCard = null;
+
+    if (matchedPairs === totalPairs) {
+        setTimeout(() => {
+         alert("YOU WON THE GAME");
+       allCards.forEach((card) => {
+        card.classList.remove("flip");
+     });
+    setTimeout(() => {
+      shuffleCards();
+    }, 500);
+    matchedPairs = 0;
+    firstCard = null;
+    secondCard = null;
+    canClick = true;
+        }, 1000);
+    }
+      } else {     
+    //handel non-matching condiotion
+    canClick = false;
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+        firstCard = null;
+        secondCard = null;
+        canClick = true;
+    }, 1000);
+  }
+}
+function shuffleCards() {
+  const container = document.querySelector(".game-container");
+  const cardsArray = Array.from(allCards);
+
+  cardsArray.sort(() => Math.random() - 0.5);
+  cardsArray.forEach(card => container.appendChild(card));
+}
